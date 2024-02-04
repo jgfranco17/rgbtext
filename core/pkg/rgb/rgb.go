5392847f923aca17c71c0c03fa6859c38a4b702c
Joaquin Franco <chino.franco@gmail.com>
2024-02-04 21:24:46 +0900

    fix: remove struct in favor of functional approach

--- core/pkg/rgb/rgb.go
+++ core/pkg/rgb/rgb.go
@@ -5,26 +5,18 @@ import (
 	"math"
 )
 
-type Color struct {
-	Red   int
-	Green int
-	Blue  int
-}
-
-func createColor(i int) *Color {
+func createColor(i int) (int, int, int) {
 	f := 0.1
-	return &Color{
-		Red:   int(math.Sin(f*float64(i)+0)*127 + 128),
-		Green: int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
-		Blue:  int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128),
-	}
+	r := int(math.Sin(f*float64(i)+0)*127 + 128)
+	g := int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128)
+	b := int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+	return r, g, b
 }
 
 func Print(output []rune) {
-	for j := 0; j < len(output); j++ {
-		color := createColor(j)
-		r, g, b := color.Red, color.Green, color.Blue
-		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	for char := 0; char < len(output); char++ {
+		r, g, b := createColor(char)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[char])
 	}
 	fmt.Println()
 }
